88cb185f46a7cf256e3b0b0df92edf58f389dd97
Mihir Kalvakaalva <mihir.38k@gmail.com>
2024-08-03 00:30:10 -0400

    adding initial raman program

--- grapher.py
+++ grapher.py
@@ -3,54 +3,54 @@ from imports import *
 
 
 #main function for the graphing of raman spectra data
-def grapher(folder=''):
 
-    #identifying csv files within the folder
-    files = [file for file in pathlib.Path(folder).glob("*.csv")]
+#input folder
+folder=r"D:\Data\Raman\0802"
 
-    #correcting matplotlib presets
-    matplotlib.rcParams.update({'font.size': 18})
+#identifying csv files within the folder
+files = [file for file in pathlib.Path(folder).glob("*.csv")]
 
-    #removing temporary files
-    for file in files:
-        if "~" in str(file):
-            files.remove(file)
+#correcting matplotlib presets
+matplotlib.rcParams.update({'font.size': 18})
 
-    #looping through all files
-    for file in files:
+#removing temporary files
+for file in files:
+    if "~" in str(file):
+        files.remove(file)
 
-        #updating user of status
-        print(f'Reading from {file}')
-        print('Processing your files...')
+#looping through all files
+for file in files:
 
-        #reading excel data for cycle index and current capacity
-        df = pd.read_csv(file, header=105)
+    #updating user of status
+    print(f'Reading from {file}')
+    print('Processing your files...')
 
-        y_axis = df['Dark Subtracted #1'] - df['RelativeIntensityCorrection_Ratio #1']
-        x_axis = df['Raman Shift']
+    #reading excel data for cycle index and current capacity
+    df = pd.read_csv(file, header=105)
 
-        #correcting for series height and stacking
+    y_axis = df['Dark Subtracted #1'] - df['RelativeIntensityCorrection_Ratio #1']
+    x_axis = df['Raman Shift']
+
+    #correcting for series height and stacking
 
 
-        #creating a plot
-        series = str(pathlib.Path(file).stem)
-        #plt.xlim(400,675)
-        plt.ylim(0,20000)
-        plt.plot(x_axis, y_axis,
-                 marker='',
-                 label=series)
-        plt.legend(loc='lower right')
+    #creating a plot
+    series = str(pathlib.Path(file).stem)
+    #plt.xlim(400,675)
+    plt.ylim(0,20000)
+    plt.plot(x_axis, y_axis,
+             marker='',
+             label=series)
+    plt.legend(loc='lower right')
 
-        n=1
-        stack=0
-        print(f'Data set {n}/{len(files)} complete.')
-        n+=1
-        stack+=4000
+    n=1
+    stack=0
+    print(f'Data set {n}/{len(files)} complete.')
+    n+=1
+    stack+=4000
 
-    #assigned constant values
-    plt.ylabel(r"Intensity (a.u.)")
-    plt.xlabel("Raman Shift (cm^-1)")
-    plt.title(str(pathlib.Path(folder).stem))
-    plt.show()
-
-grapher(folder=r"D:\Data\Raman\0802")
+#assigned constant values
+plt.ylabel(r"Intensity (a.u.)")
+plt.xlabel("Raman Shift (cm^-1)")
+plt.title(str(pathlib.Path(folder).stem))
+plt.show()
